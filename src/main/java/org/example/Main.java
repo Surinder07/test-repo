fb723bd5daa3ded0cdaea15f5aa6da0b1ca0a312
surinderpragra <surinder.singh0322@gmail.com>
2023-02-09 12:24:52 -0500

    added branch

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int number, i, evenSum = 0, oddSum = 0;
+        sc = new Scanner(System.in);
+
+        System.out.print(" Please Enter any Number : ");
+        number = sc.nextInt();
+
+        for(i = 1; i <= number; i++)
+        {
+            if(i % 2 == 0)
+            {
+                evenSum = evenSum + i;
+            }
+            else
+            {
+                oddSum = oddSum + i;
+            }
+        }
+        System.out.println("\n The Sum of Even Numbers upto " + number + "  =  " + evenSum);
+        System.out.println("\n The Sum of Odd Numbers upto " + number + "  =  " + oddSum);
+    }
+}
